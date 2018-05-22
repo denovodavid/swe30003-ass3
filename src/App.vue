41fcfd814a2cc288949eef58840acceb99223910
smeagolem <stooge213@hotmail.com>
2018-05-22 18:46:37 +1000

    :lipstick: update default table style

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
+table {
+  border-collapse: collapse;
+}
+
+th, td {
+  text-align: left;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 0.3em;
+}
 </style>
