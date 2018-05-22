14a8417e16494f039655de2236862fc3c3f95ce6
smeagolem <stooge213@hotmail.com>
2018-05-22 12:58:34 +1000

    :bento: add menu page

---
+++ src/pages/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <menu-bar/>
+  <h1>Menu Page</h1>
+</div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar'
+
+export default {
+  name: 'menu-page',
+  components: {
+    MenuBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
