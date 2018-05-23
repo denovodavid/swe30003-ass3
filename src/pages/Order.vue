972c5f6ef897425c6f49d2c82642988d7c34e3f0
smeagolem <stooge213@hotmail.com>
2018-05-24 09:54:48 +1000

    :lipstick: bootstrapify order page

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -1,138 +1,250 @@
 <template>
-<div>
-  <menu-bar/>
-  <h1>Order Page</h1>
-  <h2>Orders</h2>
-  <div>
-    <label>Type:</label>
-    <select v-model.trim="order.type">
-      <option value="sitting">Dine In</option>
-      <option value="takeaway">Take Away</option>
-    </select>
-  </div>
-  <div v-show="order.type === 'sitting'">
-    <label>Table:</label>
-    <select v-model.number="order.table">
-      <option v-for="n in 24" :key="n">{{ n }}</option>
-    </select>
-  </div>
-  <div v-show="order.type === 'takeaway'">
-    <label>Name:</label>
-    <input type="text" v-model.trim="order.name">
-  </div>
-  <div v-show="order.type === 'takeaway'">
-    <label>Phone:</label>
-    <input type="text" v-model.trim="order.phone">
-  </div>
-  <div>
-   <label>Items:</label>
-    <select multiple v-model="order.items">
-      <option
-        v-for="item in items"
-        :key="item['.key']"
-        :value="item['.key']"
-      >
-        {{ item.name }}
-      </option>
-    </select>
-  </div>
-  <button @click="addOrder">Add Order</button>
-  <table>
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Table</th>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>Items</th>
-        <th>Timer</th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="order in activeOrders"
-        :key="order['.key']"
-        :style="{
-          background: order.state === 'ready' && order.overdue
-            ? 'orange'
-            : order.overdue
-            ? 'red'
-            : order.state === 'ready'
-            ? 'green'
-            : ''
-        }"
-      >
-        <td>{{ order.type }}</td>
-        <td>{{ order.table }}</td>
-        <td>{{ order.name }}</td>
-        <td>{{ order.phone }}</td>
-        <td>{{ order.items.length }} Items</td>
-        <td>{{ order.timer | time }}</td>
-        <td><button @click="readyOrder(order)">Ready</button></td>
-        <td><button @click="completeOrder(order)">Complete</button></td>
-        <td><button @click="editOrder(order)">Edit</button></td>
-        <td><button @click="$store.dispatch('order/removeOrder', order)">Remove</button></td>
-      </tr>
-      <tr v-for="order in completedOrders" :key="order['.key']" :style="{ background: '#eee' }">
-        <td>{{ order.type }}</td>
-        <td>{{ order.table }}</td>
-        <td>{{ order.name }}</td>
-        <td>{{ order.phone }}</td>
-        <td>{{ order.items.length }} Items</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td><button @click="$store.dispatch('order/removeOrder', order)">Remove</button></td>
-      </tr>
-    </tbody>
-  </table>
+<b-container>
+  <br>
+  <h1>Orders</h1>
+  <!-- TODO: fix validation -->
+  <b-form @submit.stop.prevent="addOrder" @reset="clearOrder">
+    <b-form-group label="Type">
+      <b-form-select
+        v-model="order.type"
+        :options="[
+          { value: 'sitting', text: 'Dine In' },
+          { value: 'takeaway', text: 'Take Away' }
+        ]"
+        required
+      />
+    </b-form-group>
+    <b-form-group label="Table" v-show="order.type === 'sitting'">
+      <b-form-select
+        v-model="order.table"
+        :options="tables"
+      />
+    </b-form-group>
+    <b-form-group label="Name" v-show="order.type === 'takeaway'">
+      <b-form-input
+        type="text"
+        v-model.trim="order.name"
+        placeholder="Ryan KS"
+      />
+    </b-form-group>
+    <b-form-group label="Phone" v-show="order.type === 'takeaway'">
+      <b-form-input
+        type="text"
+        v-model.trim="order.phone"
+        placeholder="0987654321"
+      />
+    </b-form-group>
+    <b-form-group label="Items">
+      <b-form-select
+        v-model="order.items"
+        :options="items.map(x => ({ value: x['.key'], text: x.name }))"
+        multiple
+        required
+      />
+    </b-form-group>
+    <b-button type="submit" variant="primary">Add Order</b-button>
+    <b-button
+      type="reset"
+      variant="outline-danger"
+      class="float-right"
+    >
+      Reset
+    </b-button>
+  </b-form>
+  <br>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Active" active>
+        <b-list-group>
+          <b-list-group-item
+            v-for="order in activeOrders"
+            :key="order['.key']"
+            :variant="
+              order.state === 'ready' && order.overdue
+                ? 'warning'
+                : order.overdue
+                ? 'danger'
+                : order.state === 'ready'
+                ? 'primary'
+                : ''
+            "
+            class="flex-column align-items-start"
+          >
+            <div class="d-flex w-100 justify-content-between mb-2">
+              <h2 class="mb-1">
+                <b-badge
+                  :variant="
+                    order.state === 'ready' && order.overdue
+                      ? 'warning'
+                      : order.overdue
+                      ? 'danger'
+                      : order.state === 'ready'
+                      ? 'primary'
+                      : ''
+                  "
+                >
+                  {{ order.timer | time }}
+                </b-badge>
+              </h2>
+              <h5>{{ order.type === 'sitting' ? 'Dine In' : 'Take Away' }}</h5>
+            </div>
+            <div class="d-flex w-100 justify-content-between">
+              <div>
+                <p class="mb-1" v-show="order.type === 'sitting'">
+                  <b>Table {{ order.table }}</b>
+                </p>
+                <p class="mb-1" v-show="order.type === 'takeaway'">
+                  <b>{{ order.name }} &#183; {{ order.phone }}</b>
+                </p>
+                <ul>
+                  <!-- TODO: make this more efficient -->
+                  <li v-for="item in order.items" :key="item">
+                    {{ items.find(x => x['.key'] === item).name }}
+                  </li>
+                </ul>
+              </div>
+              <b-button-group class="align-self-end">
+                <b-button
+                  @click="readyOrder(order)"
+                  :pressed="order.state === 'ready'"
+                  size="sm"
+                  variant="outline-primary"
+                >
+                  Ready
+                </b-button>
+                <b-button
+                  @click="completeOrder(order)"
+                  size="sm"
+                  variant="outline-success"
+                >
+                  Complete
+                </b-button>
+                <b-button
+                  @click="editOrder(order)"
+                  size="sm"
+                  variant="outline-secondary"
+                >
+                  Edit
+                </b-button>
+                <!-- TODO: add popover confirmation -->
+                <b-button
+                  @click="$store.dispatch('order/removeOrder', order)"
+                  size="sm"
+                  variant="outline-danger"
+                  class="float-right"
+                >
+                  Remove
+                </b-button>
+              </b-button-group>
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+      </b-tab>
+      <b-tab title="Complete">
+        <b-list-group>
+          <b-list-group-item
+            v-for="order in completedOrders"
+            :key="order['.key']"
+            :variant="
+              order.state === 'ready' && order.overdue
+                ? 'warning'
+                : order.overdue
+                ? 'danger'
+                : order.state === 'ready'
+                ? 'primary'
+                : ''
+            "
+            class="flex-column align-items-start"
+          >
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">
+                Served &#183;
+                <small>{{ order.type === 'sitting' ? 'Dine In' : 'Take Away' }}</small>
+              </h5>
+              <h5>{{ order.items.length }} Item{{ order.items.length > 1 ? 's' : '' }}</h5>
+            </div>
+            <div class="d-flex w-100 justify-content-between">
+              <div>
+                <p class="mb-1" v-show="order.type === 'sitting'">
+                  <b>Table {{ order.table }}</b>
+                </p>
+                <p class="mb-1" v-show="order.type === 'takeaway'">
+                  <b>{{ order.name }} &#183; {{ order.phone }}</b>
+                </p>
+                <ul>
+                  <!-- TODO: make this more efficient -->
+                  <li v-for="item in order.items" :key="item">
+                    {{ items.find(x => x['.key'] === item).name }}
+                  </li>
+                </ul>
+              </div>
+              <b-button-group class="align-self-end">
+                <!-- TODO: add popover confirmation -->
+                <b-button
+                  @click="$store.dispatch('order/removeOrder', order)"
+                  size="sm"
+                  variant="outline-danger"
+                  class="float-right"
+                >
+                  Remove
+                </b-button>
+              </b-button-group>
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+      </b-tab>
+    </b-tabs>
+  </b-card>
 
-  <modal
+  <!-- TODO: fix form validation -->
+  <b-modal
+    ref="editOrderModal"
+    @ok="$event.preventDefault(); updateOrder(editableOrder)"
     title="Edit Order"
-    :open="editableOrderOpen"
-    @close="editableOrderOpen = false"
   >
-    <div>
-      <label>Type:</label>
-      <select v-model.trim="editableOrder.type">
-        <option value="sitting">Dine In</option>
-        <option value="takeaway">Take Away</option>
-      </select>
-    </div>
-    <div v-show="editableOrder.type === 'sitting'">
-      <label>Table:</label>
-      <select v-model.number="editableOrder.table">
-        <option v-for="n in 24" :key="n">{{ n }}</option>
-      </select>
-    </div>
-    <div v-show="editableOrder.type === 'takeaway'">
-      <label>Name:</label>
-      <input type="text" v-model.trim="editableOrder.name">
-    </div>
-    <div v-show="editableOrder.type === 'takeaway'">
-      <label>Phone:</label>
-      <input type="text" v-model.trim="editableOrder.phone">
-    </div>
-    <div>
-    <label>Items:</label>
-      <select multiple v-model="editableOrder.items">
-        <option
-          v-for="item in items"
-          :key="item['.key']"
-          :value="item['.key']"
-        >
-          {{ item.name }}
-        </option>
-      </select>
-    </div>
-    <button @click="updateOrder(editableOrder)">Save</button>
-  </modal>
+    <b-form>
+      <b-form-group label="Type">
+        <b-form-select
+          v-model="editableOrder.type"
+          :options="[
+            { value: 'sitting', text: 'Dine In' },
+            { value: 'takeaway', text: 'Take Away' }
+          ]"
+          required
+        />
+      </b-form-group>
+      <b-form-group label="Table" v-show="editableOrder.type === 'sitting'">
+        <b-form-select
+          v-model="editableOrder.table"
+          :options="tables"
+        />
+      </b-form-group>
+      <b-form-group label="Name" v-show="editableOrder.type === 'takeaway'">
+        <b-form-input
+          type="text"
+          v-model.trim="editableOrder.name"
+          placeholder="Ryan KS"
+        />
+      </b-form-group>
+      <b-form-group label="Phone" v-show="editableOrder.type === 'takeaway'">
+        <b-form-input
+          type="text"
+          v-model.trim="editableOrder.phone"
+          placeholder="0987654321"
+        />
+      </b-form-group>
+      <b-form-group label="Items">
+        <b-form-select
+          v-model="editableOrder.items"
+          :options="items.map(x => ({ value: x['.key'], text: x.name }))"
+          multiple
+          required
+        />
+      </b-form-group>
+    </b-form>
+  </b-modal>
 
-</div>
+</b-container>
 </template>
 
 <script>
@@ -170,11 +282,11 @@ export default {
   data () {
     return {
       order: new Order(),
-      editableOrderOpen: false,
       editableOrder: {
         '.key': '',
         ...new Order()
-      }
+      },
+      tables: Array(24 - 1 + 1).fill().map((_, idx) => 1 + idx)
     }
   },
   computed: {
@@ -197,7 +309,7 @@ export default {
     },
     editOrder (order) {
       this.editableOrder = clone(order)
-      this.editableOrderOpen = true
+      this.$refs.editOrderModal.show()
     },
     async readyOrder (order) {
       // TODO: handle error
@@ -214,7 +326,7 @@ export default {
     async updateOrder (order) {
       // TODO: handle error
       await this.$store.dispatch('order/updateOrder', order)
-      this.editableOrderOpen = false
+      this.$refs.editOrderModal.hide()
     }
   }
 }
