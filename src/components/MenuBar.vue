6615f65ee70d325796108dba1efca6637b6c64c4
smeagolem <stooge213@hotmail.com>
2018-05-23 22:50:22 +1000

    :lipstick: fix menu bar width

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,20 +1,22 @@
 <template>
 <b-navbar toggleable="md" type="dark" variant="dark">
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-  <b-navbar-brand href="#">The Cuddly Wombat</b-navbar-brand>
-  <b-collapse is-nav id="nav_collapse">
-    <b-navbar-nav>
-      <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
-      <b-nav-item :to="{ name: 'Menu' }">Menu</b-nav-item>
-      <b-nav-item :to="{ name: 'Order' }">Order</b-nav-item>
-      <b-nav-item :to="{ name: 'Reservation' }">Reservation</b-nav-item>
-      <b-nav-item :to="{ name: 'Sale' }">Sale</b-nav-item>
-    </b-navbar-nav>
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item href="#">Sign In</b-nav-item>
-    </b-navbar-nav>
-  </b-collapse>
+  <b-container>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">The Cuddly Wombat</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+        <b-nav-item :to="{ name: 'Menu' }">Menu</b-nav-item>
+        <b-nav-item :to="{ name: 'Order' }">Order</b-nav-item>
+        <b-nav-item :to="{ name: 'Reservation' }">Reservation</b-nav-item>
+        <b-nav-item :to="{ name: 'Sale' }">Sale</b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Sign In</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-container>
 </b-navbar>
 </template>
 
