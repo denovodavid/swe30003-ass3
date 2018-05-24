3df9edcea1ac80d84c49065733cb46c3cc812666
smeagolem <stooge213@hotmail.com>
2018-05-24 10:28:42 +1000

    :lipstick: update home page with coffee

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,24 @@
 <template>
 <b-container>
   <br>
-  <h1>Home</h1>
+  <b-jumbotron header="The Cuddly Wombat" lead="Cafe &amp; Restaurant" >
+    <p>For more information click below</p>
+    <b-btn variant="dark" href="#">More Info</b-btn>
+  </b-jumbotron>
+  <b-card title="Coffee"
+          img-src="https://picsum.photos/1200/600?image=755"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-2">
+    <p class="card-text">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Dolore blanditiis temporibus recusandae quia, eos ducimus aliquam nisi
+      molestiae maxime! Vitae, iure. Ad, veniam incidunt? Sint harum
+      reprehenderit minus blanditiis rerum.
+    </p>
+    <b-button :to="{ name: 'Menu' }" variant="dark">Menu</b-button>
+  </b-card>
 </b-container>
 </template>
 
